ecafaa8be5e9d5443fc108fb941d2f7d232462ed
Martin Spassov <mspassov@uwo.ca>
2025-01-15 21:13:17 -0500

    Property cards completed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "@/assets/styles/globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Property Pulse | Home",
@@ -13,6 +14,7 @@ const layout = ({ children }) => {
       <body>
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
